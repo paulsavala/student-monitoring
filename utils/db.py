8ad80103a2801bf12fd1878eff2ab15650503e48
Paul Savala <paulsavala@gmail.com>
2020-07-16 20:07:16 -0500

    changed row factory setup to use psycopg2 implementation

--- utils/db.py
+++ utils/db.py
@@ -2,18 +2,17 @@ import psycopg2
 from logzero import logger
 
 
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+# def dict_factory(cursor, row):
+#     d = {}
+#     for idx, col in enumerate(cursor.description):
+#         d[col[0]] = row[idx]
+#     return d
 
 
 def create_connection(db_url):
     conn = None
     try:
-        conn = psycopg2.connect(db_url, sslmode='require')
-        conn.row_factory = dict_factory
+        conn = psycopg2.connect(db_url, sslmode='require', cursor_factory=psycopg2.extras.DictCursor)
     except psycopg2.Error as e:
         logger.error(e)
         raise psycopg2.Error('Database connection failed')
